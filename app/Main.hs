da65ed6005236d973db415aa1a83db6445b31fdd
Radcliffe <radclifferobs@gmail.com>
2019-10-02 22:23:32 -0500

    filter procs and create struct

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,49 @@
 module Main where
 
-import Lib
-import System.IO
-import System.Directory
-import Text.Read
 import Control.Monad
+import Data.Functor
 import Data.List.Split
+import Lib
+import System.Directory
+import System.IO
+import Text.Read
 
 -- main :: IO ()
 -- main = readFile "/proc/1726/cmdline" >>= putStrLn
+data Proc =
+  Proc
+    { name :: String
+    , pid :: String
+    }
+
+data Model =
+  Model
+    { procs :: [Proc]
+    , search :: String -- Maybe String ?
+    , position :: Int
+    }
+
+-- show_procs :: [String] -> String -> Int -> IO ()
+-- show_procs procs search pos =
+-- show_procs :: [String] -> IO ()
+-- show_procs procs = map put(take 5 procs)
+showProc x = putStrLn $ (pid x) ++ " :: " ++ (name x)
+
+path pid = "/proc/" ++ pid ++ "/cmdline"
+
+getProcs :: IO [Proc]
+getProcs =
+  getDirectoryContents "/proc/" >>=
+  filterM (\x ->
+                 case readMaybe x :: Maybe Integer of
+                   Just _ -> return True
+                   Nothing -> return False)
+  >>= filterM (\x -> doesFileExist (path x) )
+  >>= mapM (\pid ->
+              readFile (path pid) >>= (\name -> return (Proc (head . splitOn " " $ name) pid)))
+  >>= filterM (\x -> return (name x /= ""))
 
 main :: IO ()
-main = getDirectoryContents "/proc/"
-  >>= filterM (\x -> case readMaybe x :: Maybe Integer of
-                  Just _ -> return True
-                  Nothing -> return False)
-  >>= mapM (\x ->
-              readFile ("/proc/" ++ x ++ "/cmdline") >>= return . head . splitOn " ")
-  >>= mapM_ putStrLn
+main =
+  take 5 <$> getProcs
+  >>= mapM_ showProc
