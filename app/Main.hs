431ebdba3bfb28266c6adf1cd3d4427f56cc8a21
Radcliffe <radclifferobs@gmail.com>
2019-10-05 18:30:34 -0500

    Accept input to scroll list of procs

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
 import           Control.Monad
 import           Data.List.Split
 import           Data.Maybe
+import           System.Console.ANSI
 import           System.Directory
 import           SysTools.Tasks
 import           Text.Read
@@ -18,13 +20,17 @@ data Model =
     { procs    :: [Proc]
     , search   :: String -- Maybe String ?
     , position :: Int
+    , slot     :: Int
     }
+
+data ArrowInput = Up | Down
+data Input = Exit | Nav ArrowInput
+
+maxShown = 5
 
 header = "Filter Processes : \n"
 instructions = "\nup/down arrow keys to move, type to filter, enter to select"
 
-render :: [Proc] -> IO ()
-render ps = putStrLn $ header <> unlines (showProc <$> ps) <> instructions
 
 filter_procs :: String -> [Proc] -> [Proc]
 filter_procs search = filter (isContainedIn search . name)
@@ -46,13 +52,62 @@ getProcs = do
                           contents <- readFile . path $ pid
                           return Proc {name = getProcName contents, pid = pid}
                       ) valid_dirs
-  -- found_procs <- mapM ((>>= (\x -> return Proc {name = getProcName x, pid = ""})) . readFile . path)  valid_dirs
   return $ filter hasName found_procs
   where
     hasName x = name x /= ""
 
+-- \ESC[A is code for Up arrow etc
+parseInput :: IO Input
+parseInput =
+  getChar >>= \case
+    '\ESC' -> parseInput
+    '[' -> parseInput
+    'A' -> return $ Nav Up
+    'B' -> return $ Nav Down
+    _ -> return Exit
+
+updatePos :: ArrowInput -> Int -> Int -> Int
+updatePos dir current length =
+  case dir of
+    Up   -> if current == 1 then current else current - 1
+    Down -> if current == length then current else current + 1
+
+updateSlot :: ArrowInput -> Int -> Int -> Int
+updateSlot dir current length =
+  case dir of
+    Up   -> if current == 1 then current else current - 1
+    Down -> if current == min maxShown length then current else current + 1
+
+updateModel :: Model -> Int -> Int -> Model
+updateModel model pos slot = Model (procs model) (search model) pos slot
+
+render :: Model -> IO ()
+render model = do
+  clearScreen
+  putStrLn $ header <> unlines (showProc <$> ps) <> instructions
+  where
+    ps = take 10 . drop (position model - 1) $ (procs model)
+
+listen :: Model -> IO ()
+listen model =
+  parseInput >>= \case
+    Nav dir ->
+      let size = length $ procs model
+          currentPos = position model
+          currentSlot = slot model
+          newModel =
+            updateModel
+              model
+              (updatePos dir currentPos size)
+              (updateSlot dir currentSlot size)
+       in render newModel >> listen newModel
+    Exit -> return ()
+
 main :: IO ()
 main = do
   ps <- getProcs
-  render $ filter_procs "emacs" ps
-  -- listen
+  putStrLn $ show . length $ ps
+  render $ model ps
+  listen $ model ps
+  where
+    model x = Model x "" 1 1
